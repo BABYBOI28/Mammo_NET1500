b4a9149e5560ca5a9eb5710709fba5b7d32e173f
BABYBOI28 <168204391+BABYBOI28@users.noreply.github.com>
2024-04-26 11:32:55 -0400

    Create Lambda_JS.js
    First
    lambda

---
+++ Lambda_JS.js
@@ -0,0 +1,9 @@
+export const handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello world from MAmmo Yohannis Lambda!'),
+
+    };
+    return response;
+};
